6b0dcfbd246e97a1e17efdb0c97a1fe0027856fc
Demonorium <Demonorium@yandex.ru>
2021-10-20 22:22:48 +0300

    Добавлены комментарии

--- backend/src/main/java/com/demonorium/database/PartOfSource.java
+++ backend/src/main/java/com/demonorium/database/PartOfSource.java
@@ -2,6 +2,14 @@ package com.demonorium.database;
 
 import com.demonorium.database.entity.Source;
 
+/**
+ * Данный интерефейс, должен быть у всех классов, на которые распространяются права доступа источника.
+ * Используются для проверки доступа к объекту.
+ */
 public interface PartOfSource {
+    /**
+     * Метод для получения источника, к которому относится объект
+     * @return источник к которому относится объект
+     */
     Source getSource();
 }
