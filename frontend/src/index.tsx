32fb89770d2b0905d1adaf91f18403ae723b4b14
Demonorium <Demonorium@yandex.ru>
2021-10-29 16:41:01 +0300

    Подключены DayJS и Redux

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 import {Provider} from "react-redux";
-import store from './store/global'
+import store from './store/store'
+import {LocalizationProvider} from "@mui/lab";
+import DateAdapter from '@mui/lab/AdapterDayjs';
+
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <LocalizationProvider dateAdapter={DateAdapter}>
+            <App />
+        </LocalizationProvider>
     </Provider>, document.querySelector('#root'));
